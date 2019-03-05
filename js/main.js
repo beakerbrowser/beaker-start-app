98a80e7fe0a683ed7f9c5c7ac367ac87407a32d1
Paul Frazee <pfrazee@gmail.com>
2019-03-05 11:04:19 -0600

    Formatting

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import '/vendor/beaker-app-stdlib/js/com/app-header/search.js'
-import "./com/blue-notice.js"
-import "./com/top-right-controls.js"
-import "./com/pinned-bookmarks.js"
+import './com/blue-notice.js'
+import './com/top-right-controls.js'
+import './com/pinned-bookmarks.js'
