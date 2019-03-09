0e8e228385b897669c4acb9988041a3d3ee4b81b
Paul Frazee <pfrazee@gmail.com>
2019-03-09 13:31:59 -0600

    Add applications section

---
+++ css/com/applications.css.js
@@ -0,0 +1,80 @@
+import {css} from '/vendor/beaker-app-stdlib/vendor/lit-element/lit-element.js'
+import commonCSS from '/vendor/beaker-app-stdlib/css/common.css.js'
+
+const cssStr = css`
+${commonCSS}
+
+h2 {
+  font-size: 11px;
+  color: rgba(0,0,0,.75);
+}
+
+h2 button {
+  outline: 0;
+  cursor: pointer;
+  float: right;
+}
+
+h2 button:hover {
+  background: #eee;
+}
+
+.applications {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 20px;
+  width: 100%;
+  user-select: none;
+}
+
+.application {
+  background: #fff;
+  text-align: center;
+  outline: 1px solid #ccc;
+  height: 120px;
+}
+
+.application:hover {
+  outline: 1px solid #ccc;
+  background: #eee;
+}
+
+.application .favicon {
+  margin-top: 30px;
+  width: 32px;
+  height: 32px;
+}
+
+.application .title {
+  text-overflow: ellipsis;
+  color: rgba(51, 51, 51, 0.9);
+  font-size: 12px;
+  max-width: 100px;
+  margin: auto;
+  margin-top: 10px;
+}
+
+@media (min-width: 640px) {
+  .applications-container .applications {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
+@media (min-width: 740px) {
+  .applications-container .applications {
+    grid-template-columns: repeat(5, 1fr);
+  }
+}
+
+@media (min-width: 900px) {
+  .applications-container .applications {
+    grid-template-columns: repeat(6, 1fr);
+  }
+}
+
+.applications-container .application.explorer-pin i {
+  font-size: 45px;
+  margin-top: 38px;
+}
+`
+export default cssStr
---
+++ js/com/applications.js
@@ -0,0 +1,92 @@
+import { LitElement, html } from '/vendor/beaker-app-stdlib/vendor/lit-element/lit-element.js'
+import * as contextMenu from '/vendor/beaker-app-stdlib/js/com/context-menu.js'
+import * as toast from '/vendor/beaker-app-stdlib/js/com/toast.js'
+import applicationsCSS from '../../css/com/applications.css.js'
+
+class Applications extends LitElement {
+  static get properties() {
+    return {
+      shouldShow: {type: Boolean}
+    }
+  }
+
+  constructor () {
+    super()
+    this.shouldShow = false
+    this.load()
+  }
+
+  async load () {
+    this.shouldShow = (await beaker.browser.getSetting('start_section_hide_applications')) !== 1
+  }
+
+  // rendering
+  // =
+
+  render() {
+    if (!this.shouldShow) {
+      return html`<div></div>`
+    }
+    return html`
+      <link rel="stylesheet" href="/vendor/beaker-app-stdlib/css/fontawesome.css">
+      <div class="applications-container">
+        <h2>
+          <span class="far fa-window-maximize"></span>
+          Applications
+          <button @click=${this.onClickManagerDropdown}><span class="fas fa-ellipsis-h"></span></button>
+        </h2>
+        <div class="applications">
+          <a class="application" href="beaker://bookmarks">
+            <img class="favicon" src="beaker-favicon:64,beaker://bookmarks">
+            <div class="title">Bookmarks</div>
+          </a>
+          <a class="application" href="beaker://library">
+            <img class="favicon" src="beaker-favicon:64,beaker://library">
+            <div class="title">Library</div>
+          </a>
+          <a class="application" href="dat://beaker.social">
+            <img class="favicon" src="beaker-favicon:64,dat://beaker.social">
+            <div class="title">Beaker.Social</div>
+          </a>
+        </div>
+      </div>
+    `
+  }
+
+  // events
+  // =
+
+  onClickManagerDropdown (e) {
+    e.stopPropagation()
+    contextMenu.create({
+      x: e.currentTarget.getBoundingClientRect().right,
+      y: e.currentTarget.getBoundingClientRect().bottom + document.documentElement.scrollTop,
+      right: true,
+      noBorders: true,
+      items: [
+        {icon: 'fas fa-times', label: 'Remove section', click: () => this.onRemoveSection()}
+      ],
+      style: 'padding: 4px 0;',
+      fontAwesomeCSSUrl: '/vendor/beaker-app-stdlib/css/fontawesome.css'
+    })
+  }
+
+  async onRemoveSection () {
+    await beaker.browser.setSetting('start_section_hide_applications', 1)
+    this.shouldShow = false
+
+    const undo = async () => {
+      await beaker.browser.setSetting('start_section_hide_applications', 0)
+      this.shouldShow = true
+    }
+
+    toast.create('Section removed', '', 10e3, {label: 'Undo', click: undo})
+  }
+}
+
+// styles
+// =
+
+Applications.styles = applicationsCSS
+
+customElements.define('start-applications', Applications);
